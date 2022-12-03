674a7e7963c289ee25ea18963d0a8c277ccb30e2
chleibig <leibig.christian@gmail.com>
2022-12-03 15:17:43 +0100

    Day3: 2nd half

--- day3.py
+++ day3.py
@@ -1,4 +1,5 @@
 import string
+from typing import List
 from util import read_file
 
 
@@ -16,8 +17,22 @@ def score_rucksack(rucksack: str) -> int:
     return priority(get_shared_item(rucksack))
 
 
+def get_rucksack_groups(rucksacks: List[str]) -> List[List[str]]:
+    return [rucksacks[i : i + 3] for i in range(0, len(rucksacks), 3)]
+
+
+def get_badge_item(rucksack_group: List[str]) -> str:
+    assert len(rucksack_group) == 3
+    return set.intersection(*[set(r) for r in rucksack_group]).pop()
+
+
+def score_rucksack_group(rucksack_group: List[str]) -> int:
+    return priority(get_badge_item(rucksack_group))
+
+
 if __name__ == "__main__":
 
+    print("Part one")
     test_rucksacks = [
         "vJrwpWtwJgWrhcsFMMfFFhFp",
         "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
@@ -32,3 +47,16 @@ if __name__ == "__main__":
 
     rucksacks = read_file("input_day3.txt")
     print(f"Total priority is {sum(score_rucksack(r) for r in rucksacks)}")
+
+    print("Part two")
+    test_rucksacks = [
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+    ]
+    assert sum(score_rucksack_group(rg) for rg in get_rucksack_groups(test_rucksacks)) == 70
+
+    print(f"Total priority is {sum(score_rucksack_group(rg) for rg in get_rucksack_groups(rucksacks))}")
