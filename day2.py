24051445c75b98c723c0fc83dc8f0c26f5a946d0
chleibig <leibig.christian@gmail.com>
2022-12-03 14:07:36 +0100

    Day2: 2nd half

--- day2.py
+++ day2.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Tuple
+from typing import Optional, Tuple
 from util import read_file
 
 
@@ -10,12 +10,30 @@ class Choice(Enum):
 
 
 opponent_map = {"A": Choice.ROCK, "B": Choice.PAPER, "C": Choice.SCISSORS}
-my_map = {"X": Choice.ROCK, "Y": Choice.PAPER, "Z": Choice.SCISSORS}
+my_part_one_map = {"X": Choice.ROCK, "Y": Choice.PAPER, "Z": Choice.SCISSORS}
+my_part_two_map = {"X": 0, "Y": 3, "Z": 6}
 
 
-def decode_round(encoded_round: str) -> Tuple[Choice, Choice]:
-    o_choice, m_choice = encoded_round.split(" ")
-    return opponent_map[o_choice], my_map[m_choice]
+def decode_my_choice(m_key: str, o_key: Optional[str] = None) -> Choice:
+    if o_key is None:
+        return my_part_one_map[m_key]
+    # My choice depends on the opponents choice as I need to choose
+    # such that the desired outcome is achieved
+    desired_outcome = my_part_two_map[m_key]
+    o_choice = opponent_map[o_key]
+    m_choice = [mc for mc in Choice if score_outcome(o_choice, mc) == desired_outcome]
+    assert len(m_choice) == 1
+    return m_choice[0]
+
+
+def decode_round(encoded_round: str, strategy: str) -> Tuple[Choice, Choice]:
+    o_key, m_key = encoded_round.split(" ")
+    if strategy == "part_one":
+        return opponent_map[o_key], decode_my_choice(m_key)
+    elif strategy == "part_two":
+        return opponent_map[o_key], decode_my_choice(m_key, o_key)
+    else:
+        raise ValueError
 
 
 def score_outcome(opponent: Choice, me: Choice) -> int:
@@ -41,9 +59,12 @@ def score_round(opponent: Choice, me: Choice) -> int:
 
 if __name__ == "__main__":
 
-    assert score_round(*decode_round("A Y")) == 8
-    assert score_round(*decode_round("B X")) == 1
-    assert score_round(*decode_round("C Z")) == 6
+    # Assert the provided examples are working as expected
+    for strategy, scores in zip(["part_one", "part_two"], [[8, 1, 6], [4, 1, 7]]):
+        for encoded, score in zip(["A Y", "B X", "C Z"], scores):
+            assert score_round(*decode_round(encoded, strategy)) == score
 
     rounds = read_file("input_day2.txt")
-    print(f"Total score is: {sum([score_round(*decode_round(r)) for r in rounds])}")
+    for strategy in ["part_one", "part_two"]:
+        print(strategy)
+        print(f"Total score is: {sum([score_round(*decode_round(r, strategy)) for r in rounds])}")
