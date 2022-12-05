b509522556aff3e1493e52b2b187819f9d5f5190
chleibig <leibig.christian@gmail.com>
2022-12-05 09:06:22 +0100

    Day5: 2nd half

--- day5.py
+++ day5.py
@@ -2,8 +2,31 @@ from collections import defaultdict
 from typing import Dict, List
 from util import read_file
 
+
 Stacks = Dict[int, List[str]]
 Instructions = List[str]
+
+
+class CrateMover:
+    @staticmethod
+    def rearrange(stacks: Stacks, n_move: int, from_stack: int, to_stack: int) -> Stacks:
+        raise NotImplementedError
+
+
+class CrateMover9000(CrateMover):
+    @staticmethod
+    def rearrange(stacks: Stacks, n_move: int, from_stack: int, to_stack: int) -> Stacks:
+        for _ in range(n_move):
+            stacks[to_stack].append(stacks[from_stack].pop())
+        return stacks
+
+
+class CrateMover9001(CrateMover):
+    @staticmethod
+    def rearrange(stacks: Stacks, n_move: int, from_stack: int, to_stack: int) -> Stacks:
+        stacks[to_stack].extend(stacks[from_stack][-n_move:])
+        del stacks[from_stack][-n_move:]
+        return stacks
 
 
 def parse_stacks(crates_and_instructions: List[str]) -> Stacks:
@@ -29,11 +52,10 @@ def parse_instructions(crates_and_instructions: List[str]) -> Instructions:
     return crates_and_instructions[crates_and_instructions.index("") + 1 :]
 
 
-def rearrange(stacks: Stacks, instructions: Instructions) -> Stacks:
+def rearrange(stacks: Stacks, instructions: Instructions, crate_mover: CrateMover) -> Stacks:
     for instruction in instructions:
         n_move, from_stack, to_stack = map(int, instruction.split(" ")[1::2])
-        for _ in range(n_move):
-            stacks[to_stack].append(stacks[from_stack].pop())
+        stacks = crate_mover.rearrange(stacks, n_move, from_stack, to_stack)
     return stacks
 
 
@@ -41,5 +63,5 @@ if __name__ == "__main__":
     crates_and_instructions = read_file("input/day5.txt")
     stacks = parse_stacks(crates_and_instructions)
     instructions = parse_instructions(crates_and_instructions)
-    stacks = rearrange(stacks, instructions)
+    stacks = rearrange(stacks, instructions, CrateMover9001())
     print(f"Top crates: {''.join([crates[-1] for crates in stacks.values()])}")
