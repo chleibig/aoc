ea5ce60b6918a3e3ae22525b60b05ad5d84fe0f0
chleibig <leibig.christian@gmail.com>
2022-12-06 20:40:40 +0100

    Day6: 2nd half

--- day6.py
+++ day6.py
@@ -1,7 +1,7 @@
 from util import read_file
 
 
-def test_data():
+def test_packet():
     return {
         "bvwbjplbgvbhsrlpgdmjqwftvncz": 5,
         "nppdvjthqldpwncqszvftbrmjlhg": 6,
@@ -10,11 +10,21 @@ def test_data():
     }
 
 
-def get_start_of_packet_pos(datastream: str) -> int:
+def test_message():
+    return {
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb": 19,
+        "bvwbjplbgvbhsrlpgdmjqwftvncz": 23,
+        "nppdvjthqldpwncqszvftbrmjlhg": 23,
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg": 29,
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw": 26,
+    }
+
+
+def get_start_of_packet_pos(datastream: str, length: int) -> int:
     start = 0
-    end = start + 4
+    end = start + length
     while datastream[start:end]:
-        if len(set(datastream[start:end])) == 4:
+        if len(set(datastream[start:end])) == length:
             return end
         else:
             start += 1
@@ -23,11 +33,18 @@ def get_start_of_packet_pos(datastream: str) -> int:
 
 if __name__ == "__main__":
 
-    for datastream, pos in test_data().items():
-        assert get_start_of_packet_pos(datastream) == pos
+    for datastream, pos in test_packet().items():
+        assert get_start_of_packet_pos(datastream, 4) == pos
+
+    for datastream, pos in test_message().items():
+        assert get_start_of_packet_pos(datastream, 14) == pos
 
     for datastream in read_file("input/day6.txt"):
         print(
-            f"{get_start_of_packet_pos(datastream)} characters need to be processed before "
+            f"{get_start_of_packet_pos(datastream, 4)} characters need to be processed before "
             f"the first start-of-packet marker is detected"
+        )
+        print(
+            f"{get_start_of_packet_pos(datastream, 14)} characters need to be processed before "
+            f"the first start-of-message marker is detected"
         )
